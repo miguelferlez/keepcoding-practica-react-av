d49cc7850c1bfa5d3732e70c5b2dddb7ec34e79c
Miguel Fernandez <miguelferlez@gmail.com>
2025-08-11 13:12:27 +0200

    set up Vitest and testing libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPath from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPath()],
+  css: {
+    modules: {
+      generateScopedName: "[name]_[local]_[hash:base64:5]",
+    },
+  },
+  test: {
+    globals: true,
+    coverage: {
+      provider: "v8",
+    },
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+  },
 });
